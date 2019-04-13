2db28807f184f0c847a4acfe1b4e83192cbd15d3
Zemuldo <danstan.otieno@gmail.com>
2019-04-13 14:02:57 +0300

    adds grid, filter

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,13 +2,40 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_add_squares
   end
 
-  def pick_color(%Identicon.Image{hex: [ r, g, b | _tail ]} = image) do
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def filter_add_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn ({code, _index}) ->
+      rem(code, 2) == 0
+   end
+
+   %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
-    %Identicon.Image{hex: :crypto.hash(:md5, input) |> :binary.bin_to_list}
+    %Identicon.Image{hex: :crypto.hash(:md5, input) |> :binary.bin_to_list()}
   end
 end
