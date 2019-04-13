f041e6dff9a950af776b81e6c968fab16f089fce
Zemuldo <danstan.otieno@gmail.com>
2019-04-13 14:55:53 +0300

    adds full identicons module

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,25 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_add_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.mkdir_p("./identicons")
+    File.write("./identicons/#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{pixel_map: pixel_map, color: color}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
   end
 
   def mirror_row(row) do
@@ -12,12 +31,27 @@ defmodule Identicon do
     row ++ [second, first]
   end
 
-  def filter_add_squares(%Identicon.Image{grid: grid} = image) do
-    grid = Enum.filter grid, fn ({code, _index}) ->
-      rem(code, 2) == 0
-   end
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn {_code, index} ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
 
-   %Identicon.Image{image | grid: grid}
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def filter_add_squares(%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter(grid, fn {code, _index} ->
+        rem(code, 2) == 0
+      end)
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
