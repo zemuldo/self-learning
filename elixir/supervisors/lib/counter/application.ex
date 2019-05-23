b93db01bdd54018bc6cb62b8fae0bedef4efb4cd
Danstan Onyango <danstan.otieno@gmail.com>
2019-05-23 12:24:50 +0300

    adds restructire elixir

---
+++ elixir/supervisors/lib/counter/application.ex
@@ -0,0 +1,23 @@
+defmodule Counter.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Counter.Worker.start_link(arg)
+      # {Counter.Worker, arg}
+      Counter.One,
+      Counter.Two,
+      Counter.Three
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Counter.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
